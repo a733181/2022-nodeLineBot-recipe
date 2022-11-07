c2acbb0122f04d4a9ad60b849da32fbbaea2dffd
Syuan <a733181@gmail.com>
2022-11-07 13:14:34 +0800

    新增機器人

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 2022-nodeLineBox- recipe
+# 2022-nodeLineBot-test-2
+
+## 說明
+
+---
+
+- 輸入說明跳出說明文件
+- 點選選單
+- 輸入食材名稱搜尋食材
+  - 預設為第一頁 ex:蜂蜜
+  - 搜尋二頁以上(食材名稱 頁數) ex:蜂蜜 2
+
+## 來源
+
+---
+
+[食譜網站-愛料理](https://icook.tw/)  
+[icon-flaticon](https://www.flaticon.com/)  
+[font-Google](https://fonts.google.com/specimen/RocknRoll+One?query=RocknRoll&preview.text=%E4%BB%8A%E6%97%A5%E7%A4%BE%E7%BE%A4%E8%A9%B1%E9%A1%8C&preview.text_type=custom)
---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const lineBot = require('linebot');
+
+const getData = require('./commands/index');
+const illustrate = require('./template/illustrate.json');
+
+const bot = lineBot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+});
+
+bot.on('message', (e) => {
+  if (e.message.type !== 'text') return;
+  if (e.message.text === '說明') {
+    const reply = {
+      type: 'flex',
+      altText: '說明',
+      contents: illustrate,
+    };
+    e.reply(reply);
+  } else if (e.message.text === '今日社群話題') {
+    getData(e, '/recipes/select');
+  } else if (e.message.text === '人氣食譜') {
+    getData(e, '/recipes/popular');
+  } else if (e.message.text === '素食食譜') {
+    getData(e, '/categories/28');
+    getData(e, '/categories/28');
+  } else if (e.message.text === '新廚上菜') {
+    getData(e, '/recipes/newcomers');
+  } else if (e.message.text === '當季食材料理') {
+    getData(e, '/categories/608');
+  } else if (e.message.text === '最新食譜') {
+    getData(e, '/recipes/latest');
+  } else if (e.message.text === '測試') {
+    e.reply(e.message.text);
+  } else {
+    const page = e.message.text.split(' ')[1] || '';
+    const text = `/search/${e.message.text}/${page ? `?page=${page}` : ''}`;
+    getData(e, text);
+  }
+});
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動');
+});
